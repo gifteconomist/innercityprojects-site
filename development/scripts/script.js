7cd2d6dc4cbdcb772316f27ae5842747b5336aec
Grace Poetzinger <gracepoetzinger@gmail.com>
2016-05-22 18:34:04 -0400

    functioning nav, styles skeletoned

--- development/scripts/script.js
+++ development/scripts/script.js
@@ -1,6 +1,80 @@
 $(document).ready(function() {
+    //hamburger
     $('#hamburger').click(function() {
-      $('#hamburger').toggleClass("ham-closed")
-      $('#hamburger').toggleClass("ham-open")   
-    })
+      $('#hamburger').toggleClass("ham-open");
+      $('.side-nav').toggleClass('show');
+      $('.main-wrapper').toggleClass('move');
+      $('.hams').toggleClass('show')
+    });
+    
+    //nav scroll
+    $("#about").click(function() {
+		$('html, body').animate({
+         scrollTop: $("#about-anchor").offset().top
+      }, 200);
+	});
+    
+    $("#works").click(function() {
+		$('html, body').animate({
+         scrollTop: $("#works-anchor").offset().top
+      }, 200);
+	});
+    
+    $("#news").click(function() {
+		$('html, body').animate({
+         scrollTop: $("#news-anchor").offset().top
+      }, 200);
+	});
+    
+    $("#contact").click(function() {
+		$('html, body').animate({
+         scrollTop: $("#contact-anchor").offset().top
+      }, 600);
+	});
+    
+    //side scroll
+    $("#about2").click(function() {
+		$('html, body').animate({
+         scrollTop: $("#about-anchor").offset().top
+      }, 200);
+         $('#hamburger').toggleClass("ham-open");
+      $('.side-nav').toggleClass('show');
+      $('.main-wrapper').toggleClass('move');
+      $('.hams').toggleClass('show')
+	});
+    
+    $("#works2").click(function() {
+		$('html, body').animate({
+         scrollTop: $("#works-anchor").offset().top
+      }, 200);
+         $('#hamburger').toggleClass("ham-open");
+          $('.side-nav').toggleClass('show');
+          $('.main-wrapper').toggleClass('move');
+          $('.hams').toggleClass('show')
+	});
+    
+    $("#news2").click(function() {
+		$('html, body').animate({
+         scrollTop: $("#news-anchor").offset().top
+      }, 200);
+         $('#hamburger').toggleClass("ham-open");
+      $('.side-nav').toggleClass('show');
+      $('.main-wrapper').toggleClass('move');
+      $('.hams').toggleClass('show')
+	});
+    
+    $("#contact2").click(function() {
+		$('html, body').animate({
+         scrollTop: $("#contact-anchor").offset().top
+      }, 600);
+         $('#hamburger').toggleClass("ham-open");
+      $('.side-nav').toggleClass('show');
+      $('.main-wrapper').toggleClass('move');
+      $('.hams').toggleClass('show')
+	});
+    
+//    if($('#sticky-wrapper').hasClass('is-sticky')) {
+//        $('.nav-list').css('margin', '0');
+//        console.log('sticky!')
+//    }
 });
